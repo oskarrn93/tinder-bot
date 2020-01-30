fa6b8ff3606486c0837a60677fc16b4559d4c0ae
Oskar Rosen <oskar@oskarrosen.com>
2020-01-30 23:55:56 +0100

    code refactoring

--- index.js
+++ index.js
@@ -8,39 +8,43 @@ const CHROME_REMOTE_DEBUG_PORT = 9222
 
 const { until, By } = selenium
 
-//attach to existing running chrome instance where we are already logged in to Tinder
-const options = new chrome.Options()
-options.options_['debuggerAddress'] = `localhost:${CHROME_REMOTE_DEBUG_PORT}`
-
-chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build())
-
-const driver = new selenium.Builder()
-  .forBrowser('chrome')
-  .setChromeOptions(options)
-  .build()
-
 const likeButtonXPath = By.xpath(`//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[3]`)
 
-const getRandomTimeoutBetweenClicks = () => {
-  return Math.random() * TIMEOUT_IN_MS + TIMEOUT_IN_MS / 2 //[0,1] * 1500 + 750, fastest = 750ms, slowest = 2250ms
+const getRandomTimeoutBetweenClicks = timeout => {
+  return Math.random() * timeout + timeout / 2 //[0,1] * 1500 + 750, fastest = 750ms, slowest = 2250ms
 }
+
+const getDriver = () => {
+  //attach to existing running chrome instance where we are already logged in to Tinder
+  const options = new chrome.Options()
+  options.options_['debuggerAddress'] = `localhost:${CHROME_REMOTE_DEBUG_PORT}`
+
+  chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build())
+  const driver = new selenium.Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build()
+  return driver
+}
+
 // perform throttled swiping right / liking on Tinder
-const clickLikeButton = async () => {
+const clickLikeButton = async driver => {
   return new Promise(resolve =>
     setTimeout(async () => {
       await driver.wait(until.elementLocated(likeButtonXPath), 5000)
       driver.findElement(likeButtonXPath).click()
       resolve()
-    }, getRandomTimeoutBetweenClicks()),
+    }, getRandomTimeoutBetweenClicks(TIMEOUT_IN_MS)),
   )
 }
 
 const main = async () => {
+  const driver = getDriver()
   driver.get('https://tinder.com/')
 
   let NUMBER_OF_LIKES_PERFORMED = 0
   while (true) {
-    await clickLikeButton()
+    await clickLikeButton(driver)
     NUMBER_OF_LIKES_PERFORMED++
     console.log(`Number of likes performed ${NUMBER_OF_LIKES_PERFORMED}`)
   }
