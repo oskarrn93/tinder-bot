89b7dccab7236d7dd3808da298803dd445e4fd38
Oskar Rosen <oskar@oskarrosen.com>
2020-01-30 23:11:22 +0100

    refator  code

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ import chromedriver from 'chromedriver'
 import require from 'requirejs'
 const chrome = require('selenium-webdriver/chrome')
 
+const TIMEOUT_IN_MS = 1500
+const CHROME_REMOTE_DEBUG_PORT = 9222
+
 const { until, By } = selenium
 
+//attach to existing running chrome instance where we are already logged in to Tinder
 const options = new chrome.Options()
-options.options_['debuggerAddress'] = 'localhost:9222'
+options.options_['debuggerAddress'] = `localhost:${CHROME_REMOTE_DEBUG_PORT}`
 
 chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build())
 
@@ -14,14 +18,11 @@ const driver = new selenium.Builder()
   .forBrowser('chrome')
   .setChromeOptions(options)
   .build()
-// .withCapabilities(selenium.Capabilities.chrome())
-
-const TIMEOUT_IN_MS = 1500
 
 const likeButtonXPath = By.xpath(`//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[3]`)
 
+// the pace of clicking the like button is throttled by TIMEOUT_IN_MS
 const clickLikeButton = async () => {
-  console.log('creating promise')
   return new Promise(resolve =>
     setTimeout(async () => {
       console.log('waiting for like button')
@@ -35,8 +36,11 @@ const clickLikeButton = async () => {
 const main = async () => {
   driver.get('https://tinder.com/')
 
+  let NUMBER_OF_LIKES_PERFORMED = 0
   while (true) {
     await clickLikeButton()
+    NUMBER_OF_LIKES_PERFORMED++
+    console.log(`Number of likes performed ${NUMBER_OF_LIKES_PERFORMED}`)
   }
 }
 
